83b389429b2eee1b962a96ccdbf064a4da49b106
rzik <razzek.bouallague@gmail.com>
2015-08-04 10:08:36 +0100

    FOSUserBundle

---
+++ src/Register/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Register\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
+
+class ProfileFormType extends BaseType
+{
+
+    public function getName()
+    {
+        return 'register_user_profile';
+    }
+
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+       
+
+       
+        
+        parent::buildUserForm($builder, $options);
+        
+
+        $builder->add('current_password', 'password', array(
+            'label' => 'form.current_password',
+            'translation_domain' => 'FOSUserBundle', 'attr'  => array('class' =>'form-control' , 'id' =>'username', 'placeholder' =>'ActualPassword'),
+            'mapped' => false,
+           
+        ));
+    }
+}
+        
